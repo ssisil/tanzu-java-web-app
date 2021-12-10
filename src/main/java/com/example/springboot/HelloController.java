0a2971ce2877ec9bccbc6e5e5972c9f3009a8043
ssisil <ssisil@pivotal.io>
2021-12-10 14:00:37 -0800

    take 7

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Hello Scott from Spring Boot + Tanzu! Now with live reload. Take 6";
+		return "Hello Scott from Spring Boot + Tanzu! Now with live reload. Take 7";
 	}
 
 }
